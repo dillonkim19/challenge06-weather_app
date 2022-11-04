29d52373be87b46a5802aec28a818653222b3ef0
Dillon Kim <dillonkim@Dillons-MacBook-Air.local>
2022-11-04 02:15:22 -0700

    show weather and forecast based on city

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,14 @@
 const API_KEY = 'b80f4f4a76bd4c6a587f1efe91224ce5'
 
+var cityDate = $(".city-date") 
+var currentTemp = $(".current-temp") 
+var currentWind = $(".current-wind") 
+var currentHumidity = $(".current-humidity") 
 
+var cityInfo = $(".city-info")
+
+var forecastCards = $(".forecast-cards")
+var cardsArray = ['cards-one', 'cards-two', 'cards-three', 'cards-four', 'cards-five']
 
 function getGeoLocation(query, limit = 5) {
     return fetch(`http://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=${limit}&appid=${API_KEY}`)
@@ -10,7 +18,67 @@ function getCurrentWeather(arguments) {
     return fetch(`https://api.openweathermap.org/data/3.0/onecall?lat=${arguments.lat}&lon=${arguments.lon}&exclude=hourly,minutely,alerts&units=imperial&appid=${API_KEY}`)
 }
 
-var cityInfo= $(".city-info")
+function getDate(unixTime) {
+    var myDate = new Date(unixTime * 1000);
+    myDate = myDate.toLocaleString().split(", ")[0]
+    return myDate
+}
+// console.log(myDate.toGMTString())
+
+function displayCurrentWeather(current) {
+    cityDate.text('Atlanta ' + getDate(current.dt))
+    currentTemp.text('Temp: ' + current.temp)
+    currentWind.text('Wind: ' + current.wind_speed + ' mph')
+    currentHumidity.text('Humidity: ' + current.humidity + '%')
+}
+
+function displayForecast(daily) {
+    // for (var i = 1; i < 6; i++) {
+
+    // }
+    // <div class="card card-one" style="width: 15rem;"></div>
+    // <div class="card-body"></div>
+    for (var i = 1; i < 6; i++){
+        var card = document.createElement('div')
+        card.setAttribute("class", `card ${cardsArray[i-1]} col-sm-3 col-md-4 col-lg-2`)
+        forecastCards.append(card)
+    
+        var cardBody = document.createElement('div')
+        cardBody.setAttribute("class", "card-body")
+        card.append(cardBody)
+
+        var cardDate = document.createElement('h5')
+        cardDate.textContent = `${getDate(daily[i].dt)}`
+        cardDate.setAttribute("class", "card-title")
+        cardBody.append(cardDate)
+
+        var weatherIcon = document.createElement('img')
+        weatherIcon.src = `http://openweathermap.org/img/wn/${daily[i].weather[0].icon}.png`
+        cardBody.append(weatherIcon)
+
+        var cardTemp = document.createElement('p')
+        cardTemp.textContent = `Temp: ${daily[i].temp.day}`
+        cardTemp.setAttribute("class", "card-text")
+        cardBody.append(cardTemp)
+
+        var cardWind = document.createElement('p')
+        cardWind.textContent = `Wind: ${daily[i].wind_speed}`
+        cardWind.setAttribute("class", "card-text")
+        cardBody.append(cardWind)
+
+        var cardHumidity = document.createElement('p')
+        cardHumidity.textContent = `Humidity: ${daily[i].humidity}`
+        cardHumidity.setAttribute("class", "card-text")
+        cardBody.append(cardHumidity)
+    }
+    
+    
+
+    
+    
+}
+
+// var cityData;
 
 getGeoLocation('Atlanta')
 .then(response => response.json())
@@ -22,8 +90,10 @@ getGeoLocation('Atlanta')
     getCurrentWeather({ lat: data[0].lat, lon: data[0].lon })
     .then(weatherResponse => weatherResponse.json())
     .then(weatherData => {
-        console.log(JSON.stringify(weatherData, null, 2))
-        cityInfo.text(JSON.stringify(weatherData, null, 2))
+        // console.log(JSON.stringify(weatherData, null, 2))
+        console.log(weatherData)
+        displayCurrentWeather(weatherData.current);
+        displayForecast(weatherData.daily);
     })
     .catch(error => {
         cityInfo.text(error.message)
@@ -33,5 +103,3 @@ getGeoLocation('Atlanta')
     cityInfo.text(error.message)
 })
 
-var myDate = new Date( 1667581200 * 1000);
-console.log(myDate.toGMTString()+"<br>"+myDate.toLocaleString());
